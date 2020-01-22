68a95c11027d510d72e6ca11775cd3ef4446ee82
Eduardo Oliveira <59carloseduardo@gmail.com>
2020-01-21 22:44:29 -0300

    Criando instanciação

--- src/main/java/com/project/japi/JapiApplication.java
+++ src/main/java/com/project/japi/JapiApplication.java
@@ -1,13 +1,34 @@
 package com.project.japi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class JapiApplication {
+import com.project.japi.domain.Categoria;
+import com.project.japi.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class JapiApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JapiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null, "Redes de Computadores");
+		Categoria cat2 = new Categoria(null, "Analise de Sistema");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
--- src/main/java/com/project/japi/resources/CategoriaResourse.java
+++ src/main/java/com/project/japi/resources/CategoriaResourse.java
@@ -1,7 +1,5 @@
 package com.project.japi.resources;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
